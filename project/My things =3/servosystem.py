4359be1d4b5878e42f18702a45fd2411ff58f947
EndDotExe <hughiemck@icloud.com>
2025-08-04 11:55:43 +1000

    building the framework for a servo subsystem?

---
+++ project/My things =3/servosystem.py
@@ -0,0 +1,21 @@
+from servo import Servo
+from machine import Pin, PWM
+
+class Servo_Test:
+    def __init__(self, speed):
+        self.__speed = speed
+        ...
+
+    def Forward(self):
+        ...
+
+    def Turn_CW(self):
+        ...
+
+    def Turn_ACW(self):
+        ...
+
+    def Backward(self):
+        ...
+
+    def Self_Destruct(self):
